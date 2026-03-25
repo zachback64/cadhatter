f912ee77be59ddfacffc95e128a96f8a7b7ab4ae
zachback64 <zbackas@gmail.com>
2026-03-25 09:45:44 -0700

    feat: wire up React Router, landing and app routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Landing } from './pages/Landing'
+import { AppPage } from './pages/App'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/app" element={<AppPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
